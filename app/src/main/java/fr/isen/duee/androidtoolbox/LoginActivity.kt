c5dd3d2989e15678cd673ded468deecf9dc0de40
Allan <allan.duee@isen.yncrea.fr>
2020-01-20 15:13:39 +0100

    In LoginActivity addition of login error message and redirection to HomeActivity page

--- app/src/main/java/fr/isen/duee/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/LoginActivity.kt
@@ -1,12 +1,40 @@
 package fr.isen.duee.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import org.w3c.dom.Text
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        
+        activity_login_button.setOnClickListener {
+
+            var identifiant = activity_login_email_input.text.toString()
+            var password = activity_login_password_input.text.toString()
+
+            if(identifiant.equals("admin") && password.equals("123")) {
+
+                val message = "$identifiant est connecté"
+                Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent);
+            }
+            else {
+                if(identifiant.equals("admin")) {
+                    Toast.makeText(this@LoginActivity, "Mot de passe incorect", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(this@LoginActivity, "Identifiant incorect", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
