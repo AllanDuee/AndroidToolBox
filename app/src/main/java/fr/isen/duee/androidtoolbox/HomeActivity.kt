74d3ac17bbd9935c1b027bb305a1fa7468722566
Allan <allan.duee@isen.yncrea.fr>
2020-01-20 15:26:34 +0100

    Click on "lifecycle" button bring the user into LifeCycleActivity

--- app/src/main/java/fr/isen/duee/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,20 @@
 package fr.isen.duee.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        activity_home_lifecycle_picture.setOnClickListener {
+
+            val intent = Intent(this, LifeCycleActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
