54959059778f1a788b9f38d76e00c575dd5a90b6
Allan <allan.duee@isen.yncrea.fr>
2020-01-21 11:43:57 +0100

    Add SharedPreferences for LoginActivity page. Add Disconnect Button in HomeActivity page.

--- app/src/main/java/fr/isen/duee/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/HomeActivity.kt
@@ -1,20 +1,39 @@
 package fr.isen.duee.androidtoolbox
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    private val sharedPrefFile = "kotlinsharedpreference"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
 
         activity_home_lifecycle_picture.setOnClickListener {
 
             val intent = Intent(this, LifeCycleActivity::class.java)
             startActivity(intent);
+        }
+
+        activity_home_disconnect_button.setOnClickListener {
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.clear()
+            editor.remove("email")
+            editor.remove("password")
+            editor.commit()
+
+            this.finish()
+
+            val loginActivityIntent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(loginActivityIntent)
         }
     }
 }
--- app/src/main/java/fr/isen/duee/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/LoginActivity.kt
@@ -1,28 +1,62 @@
 package fr.isen.duee.androidtoolbox
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
-import org.w3c.dom.Text
-
 
 class LoginActivity : AppCompatActivity() {
+
+    private val sharedPrefFile = "kotlinsharedpreference"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        
+
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+
+        val editor:SharedPreferences.Editor =  sharedPreferences.edit()
+
+        val sharedEmailValue = sharedPreferences.getString("email","defaultemail")
+        val sharedPasswordValue = sharedPreferences.getString("password","defaultpassword")
+
+        if(sharedEmailValue.equals("defaultemail") && sharedPasswordValue.equals("defaultpassword")){
+            activity_login_email_input.setText("")
+            activity_login_password_input.setText("")
+
+        }else{
+            if(sharedEmailValue.equals("admin") && sharedPasswordValue.equals("123")) {
+
+                val message = "$sharedEmailValue est connecté"
+                Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+
+                this.finish()
+
+                val homeActivityIntent = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(homeActivityIntent)
+            }
+        }
+
+
         activity_login_button.setOnClickListener {
 
             var identifiant = activity_login_email_input.text.toString()
             var password = activity_login_password_input.text.toString()
 
+            editor.putString("email",identifiant)
+            editor.putString("password",password)
+            editor.apply()
+            editor.commit()
+
             if(identifiant.equals("admin") && password.equals("123")) {
 
                 val message = "$identifiant est connecté"
                 Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+
+                this.finish()
 
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent);
