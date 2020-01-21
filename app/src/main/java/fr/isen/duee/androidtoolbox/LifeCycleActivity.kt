fb67a0dbd34878b61c97f6d2a7e3438c477a1752
Allan <allan.duee@isen.yncrea.fr>
2020-01-21 09:46:06 +0100

    On LifeCycleActivity add a layout to change fragment thanks to a button. In LifeCycleActivity add a ViewPager to change fragment by swipping

--- app/src/main/java/fr/isen/duee/androidtoolbox/LifeCycleActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/LifeCycleActivity.kt
@@ -4,15 +4,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_life_cycle.*
 
+private const val NUM_PAGES = 2
+
 class LifeCycleActivity : AppCompatActivity() {
+
+    private lateinit var mPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
 
+        val fragment1 = LifeCycleFragment()
+        val fragment2 = LifeCycleSecondFragment()
+
+        mPager = findViewById(R.id.activity_lifecycle_viewpager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+
+        supportFragmentManager.beginTransaction().add(R.id.activity_lifecycle_layout, fragment1).commit()
+
+        activity_lifecycle_button.setOnClickListener {
+            if(fragment1.isVisible) {
+                supportFragmentManager.beginTransaction().replace(R.id.activity_lifecycle_layout, fragment2).commit()
+            }
+            else {
+                supportFragmentManager.beginTransaction().replace(R.id.activity_lifecycle_layout, fragment1).commit()
+            }
+        }
+
         notification("onCreate", true)
+    }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = NUM_PAGES
+
+        override fun getItem(position: Int): Fragment {
+            when (position) {
+                0 -> return LifeCycleFragment()
+                else -> return LifeCycleSecondFragment ()
+            }
+        }
     }
 
     private fun notification(message : String, IsActive : Boolean) {
---
+++ app/src/main/java/fr/isen/duee/androidtoolbox/LifeCycleFragment.kt
@@ -0,0 +1,55 @@
+package fr.isen.duee.androidtoolbox
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_life_cycle.*
+
+class LifeCycleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_life_cycle, container, false)
+    }
+
+    private fun notification(message : String, IsActive : Boolean) {
+        if (IsActive) {
+            fragment_lifecycle_text.text = message
+        }
+        else {
+            Log.d("TAG", message)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        notification("onStart", true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notification("onResume", true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        notification("onPause", true)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        notification("onStop", false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        notification("onDestroy", false)
+    }
+}
