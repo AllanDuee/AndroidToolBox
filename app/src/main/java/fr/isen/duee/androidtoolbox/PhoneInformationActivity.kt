bafb38205ffbe98358a4af90574cc701fc407ae2
Allan <allan.duee@isen.yncrea.fr>
2020-01-25 20:05:43 +0100

    Add the use of the Camera. Thanks to a popup, User can now select a picture in their Gallery or take a picture thank to the camera.

--- app/src/main/java/fr/isen/duee/androidtoolbox/PhoneInformationActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/PhoneInformationActivity.kt
@@ -3,30 +3,33 @@ package fr.isen.duee.androidtoolbox
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_phone_information.*
 
 class PhoneInformationActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.M)
+    companion object {
+        private val IMAGE_PICK_CODE_GALLERY = 1000;
+        private val PERMISSION_CODE_GALLERY = 1001;
+        private val IMAGE_CAPTURE_CODE_CAMERA = 1002;
+        private val PERMISSION_CODE_CAMERA = 1003;
+    }
+    var image_uri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_information)
 
         imageClick()
-    }
-
-    companion object {
-        //image pick code
-        private val IMAGE_PICK_CODE = 1000;
-        //Permission code
-        private val PERMISSION_CODE = 1001;
     }
 
     fun imageClick() {
@@ -35,7 +38,7 @@ class PhoneInformationActivity : AppCompatActivity() {
         }
     }
 
-    fun openGallery() {
+    fun checkAndOpenGallery() {
         //check runtime permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
@@ -43,7 +46,7 @@ class PhoneInformationActivity : AppCompatActivity() {
                 //permission denied
                 val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
                 //show popup to request runtime permission
-                requestPermissions(permissions, PERMISSION_CODE);
+                requestPermissions(permissions, PERMISSION_CODE_GALLERY);
             }
             else{
                 //permission already granted
@@ -56,11 +59,45 @@ class PhoneInformationActivity : AppCompatActivity() {
         }
     }
 
+    fun checkAndOpenCamera() {
+        //if system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                //permission was not enabled
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                //show popup to request permission
+                requestPermissions(permission, PERMISSION_CODE_CAMERA)
+            }
+            else{
+                //permission already granted
+                openCamera()
+            }
+        }
+        else{
+            //system os is < marshmallow
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE_CAMERA)
+    }
+
     private fun pickImageFromGallery() {
         //Intent to pick image
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
+        startActivityForResult(intent, IMAGE_PICK_CODE_GALLERY)
     }
 
     fun popUp() {
@@ -69,10 +106,11 @@ class PhoneInformationActivity : AppCompatActivity() {
         builder.setTitle("Veuillez séléctionner un mode :")
         builder.setNegativeButton("Gallery"){_,_ ->
             Toast.makeText(applicationContext,"You have choose the Gallery",Toast.LENGTH_SHORT).show()
-            openGallery()
+            checkAndOpenGallery()
         }
         builder.setPositiveButton("Camera"){_,_ ->
             Toast.makeText(applicationContext,"You have choose the Camera",Toast.LENGTH_SHORT).show()
+            checkAndOpenCamera()
         }
         builder.setNeutralButton("Cancel"){_,_ ->
             Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
@@ -85,11 +123,22 @@ class PhoneInformationActivity : AppCompatActivity() {
     //handle requested permission result
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when(requestCode){
-            PERMISSION_CODE -> {
+            PERMISSION_CODE_GALLERY -> {
                 if (grantResults.size >0 && grantResults[0] ==
                     PackageManager.PERMISSION_GRANTED){
                     //permission from popup granted
                     pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            PERMISSION_CODE_CAMERA -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    openCamera()
                 }
                 else{
                     //permission from popup denied
@@ -101,8 +150,12 @@ class PhoneInformationActivity : AppCompatActivity() {
 
     //handle result of picked image
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE_GALLERY){
             activity_phone_information_picture.setImageURI(data?.data)
+        }
+        else  if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            activity_phone_information_picture.setImageURI(image_uri)
         }
     }
 }
