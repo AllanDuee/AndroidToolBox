8b59b8936f8a0328e8eb425471991e630a65cffb
Allan <allan.duee@isen.yncrea.fr>
2020-01-25 18:46:08 +0100

    Add popup into PhoneInformationActivity when user click on the picture. User can choose a picture from their photo gallery

--- app/src/main/java/fr/isen/duee/androidtoolbox/PhoneInformationActivity.kt
+++ app/src/main/java/fr/isen/duee/androidtoolbox/PhoneInformationActivity.kt
@@ -1,12 +1,108 @@
 package fr.isen.duee.androidtoolbox
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_phone_information.*
 
 class PhoneInformationActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_information)
+
+        imageClick()
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
+    fun imageClick() {
+        activity_phone_information_picture.setOnClickListener {
+            popUp()
+        }
+    }
+
+    fun openGallery() {
+        //check runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED){
+                //permission denied
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                //show popup to request runtime permission
+                requestPermissions(permissions, PERMISSION_CODE);
+            }
+            else{
+                //permission already granted
+                pickImageFromGallery();
+            }
+        }
+        else{
+            //system OS is < Marshmallow
+            pickImageFromGallery();
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    fun popUp() {
+        val builder = AlertDialog.Builder(this@PhoneInformationActivity)
+
+        builder.setTitle("Veuillez séléctionner un mode :")
+        builder.setNegativeButton("Gallery"){_,_ ->
+            Toast.makeText(applicationContext,"You have choose the Gallery",Toast.LENGTH_SHORT).show()
+            openGallery()
+        }
+        builder.setPositiveButton("Camera"){_,_ ->
+            Toast.makeText(applicationContext,"You have choose the Camera",Toast.LENGTH_SHORT).show()
+        }
+        builder.setNeutralButton("Cancel"){_,_ ->
+            Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            activity_phone_information_picture.setImageURI(data?.data)
+        }
     }
 }
